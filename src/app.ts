e97dadf90023a12fb0ed4a71d8120d2204adda91
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-11-11 19:28:22 +0700

    refactor: include and run (app.ts)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,48 @@
+import express, { Express } from "express";
+import cors from 'cors';
+import { DataSource } from "typeorm";
+import morgan from "morgan";
+import "reflect-metadata";
+
+import { serverConfig } from "./config/server-config";
+import { dataConfig } from "./config/data-config";
+import { UserRoute } from "./routes/user-route";
+import { SoapRoute } from "./routes/soap-route";
+
 export class App {
+    dataSource: DataSource;
+    server: Express;
+
     constructor() {
-        console.log("App is being created!");
+        const userRoute = new UserRoute();
+        const soapRoute = new SoapRoute();
+
+        this.dataSource = new DataSource(dataConfig);
+
+        this.server = express();
+        this.server.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
+        this.server.use(
+            "/api",
+            express.json(),
+            express.urlencoded({ extended: true }),
+            morgan("combined"),
+            userRoute.getRoute(),
+            soapRoute.getRoute()
+        );
     }
 
     run() {
-        console.log("App is running!");
+        this.dataSource
+            .initialize()
+            .then(async () => {
+                this.server.listen(serverConfig.port, () => {
+                    console.log(
+                        `Server is running on port: ${serverConfig.port}`
+                    );
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 }
