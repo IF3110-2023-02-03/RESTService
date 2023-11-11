3a49cbf464151d898ed0cf1b75bb34ca821638ba
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-11-11 19:27:32 +0700

    feat: initialize controllers (soap & user)

---
+++ src/controllers/soap-controller.ts
@@ -0,0 +1,225 @@
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import { Request, Response } from "express";
+
+import { AuthRequest } from "../middlewares/authentication-middleware";
+import { soapConfig } from "../config/soap-config";
+import axios from "axios";
+import xml2js from "xml2js";
+
+interface FollowRequest {
+  creatorID: number;
+  followerID: number;
+}
+
+interface FollowData {
+  creatorID: number;
+  followerID: number;
+  creatorName: string;
+  followerName: string;
+}
+
+export class SoapController {
+  accept() {
+    return async (req: Request, res: Response) => {
+      const { token } = req as AuthRequest;
+      if (!token || !token.isAdmin) {
+        res.status(StatusCodes.UNAUTHORIZED).json({
+          message: ReasonPhrases.UNAUTHORIZED,
+        });
+        return;
+      }
+
+      // Parse request body
+      const { creatorID, followerID }: FollowRequest = req.body;
+
+      try {
+        const response = await axios.post<string>(
+          `http://${soapConfig.host}:${soapConfig.port}/api/follow`,
+          `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                            <approveFollow xmlns="http://service.moments/">
+                                <arg0 xmlns="">${creatorID}</arg0>
+                                <arg1 xmlns="">${followerID}</arg1>
+                                <arg2 xmlns="">${soapConfig.key}</arg2>
+                            </approveFollow>
+                        </Body>
+                    </Envelope>`,
+          {
+            headers: {
+              "Content-Type": "text/xml",
+            },
+          }
+        );
+        const xml = await xml2js.parseStringPromise(response.data);
+        const result =
+          xml["S:Envelope"]["S:Body"][0]["ns2:approveFollowResponse"][0]
+            .return[0];
+
+        if (result === "Follow not found") {
+          res.status(StatusCodes.NOT_FOUND).json({
+            message: result,
+          });
+          return;
+        } else if (result === "Follow accepted") {
+          res.status(StatusCodes.OK).json({
+            message: result,
+          });
+          return;
+        } else {
+          res.status(StatusCodes.BAD_REQUEST).json({
+            message: result,
+          });
+          return;
+        }
+      } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+        });
+        return;
+      }
+    };
+  }
+
+  reject() {
+    return async (req: Request, res: Response) => {
+      const { token } = req as AuthRequest;
+      if (!token || !token.isAdmin) {
+        // Endpoint hanya bisa diakses oleh admin
+        res.status(StatusCodes.UNAUTHORIZED).json({
+          message: ReasonPhrases.UNAUTHORIZED,
+        });
+        return;
+      }
+
+      // Parse request body
+      const { creatorID, followerID }: FollowRequest = req.body;
+
+      try {
+        const response = await axios.post<string>(
+          `http://${soapConfig.host}:${soapConfig.port}/api/follow`,
+          `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                            <rejectFollow xmlns="http://service.moments/">
+                                <arg0 xmlns="">${creatorID}</arg0>
+                                <arg1 xmlns="">${followerID}</arg1>
+                                <arg2 xmlns="">${soapConfig.key}</arg2>
+                            </rejectFollow>
+                        </Body>
+                    </Envelope>`,
+          {
+            headers: {
+              "Content-Type": "text/xml",
+            },
+          }
+        );
+        const xml = await xml2js.parseStringPromise(response.data);
+        const result =
+          xml["S:Envelope"]["S:Body"][0]["ns2:rejectFollowResponse"][0]
+            .return[0];
+
+        if (result === "Follow not found") {
+          res.status(StatusCodes.NOT_FOUND).json({
+            message: result,
+          });
+          return;
+        } else if (result === "Follow rejected") {
+          res.status(StatusCodes.OK).json({
+            message: result,
+          });
+          return;
+        } else {
+          res.status(StatusCodes.BAD_REQUEST).json({
+            message: result,
+          });
+          return;
+        }
+      } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+        });
+        return;
+      }
+    };
+  }
+
+  index() {
+    return async (req: Request, res: Response) => {
+      const { token } = req as AuthRequest;
+      if (!token || !token.isAdmin) {
+        // Endpoint hanya bisa diakses oleh admin
+        res.status(StatusCodes.UNAUTHORIZED).json({
+          message: ReasonPhrases.UNAUTHORIZED,
+        });
+        return;
+      }
+
+      const page = parseInt((req.query?.page || "1") as string);
+      const pageSize = parseInt((req.query?.pageSize || "5") as string);
+      let followData: FollowData[] = [];
+      try {
+        const response = await axios.post<string>(
+          `http://${soapConfig.host}:${soapConfig.port}/api/follow`,
+          `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                            <getAllReqFollow xmlns="http://service.moments/">
+                                <arg0 xmlns="">${page}</arg0>
+                                <arg1 xmlns="">${pageSize}</arg1>
+                                <arg2 xmlns="">${soapConfig.key}</arg2>
+                            </getAllReqFollow>
+                        </Body>
+                    </Envelope>`,
+          {
+            headers: {
+              "Content-Type": "text/xml",
+            },
+          }
+        );
+        const xml = await xml2js.parseStringPromise(response.data);
+        const pageCount =
+          xml["S:Envelope"]["S:Body"][0]["ns2:getAllReqFollowResponse"][0]
+            .return[0].pageCount[0];
+        if (pageCount === "0") {
+          res.status(StatusCodes.OK).json({
+            message: "No follow request found",
+            data: followData,
+            pageCount: pageCount,
+          });
+          return;
+        }
+        const results =
+          xml["S:Envelope"]["S:Body"][0]["ns2:getAllReqFollowResponse"][0]
+            .return[0].data;
+
+        if (!results) {
+          res.status(StatusCodes.OK).json({
+            message: ReasonPhrases.OK,
+            data: [],
+            totalPage: pageCount,
+          });
+          return;
+        }
+
+        results.forEach((result: any) => {
+          followData.push({
+            creatorID: result.creatorID[0],
+            followerID: result.followerID[0],
+            creatorName: result.creatorName[0],
+            followerName: result.followerName[0],
+          });
+        });
+
+        res.status(StatusCodes.OK).json({
+          message: ReasonPhrases.OK,
+          data: followData,
+          totalPage: pageCount,
+        });
+        return;
+      } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+        });
+        return;
+      }
+    };
+  }
+}
---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,183 @@
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+import {
+    AuthToken,
+    AuthRequest,
+} from "../middlewares/authentication-middleware";
+import { cacheConfig } from "../config/cache-config";
+import { jwtConfig } from "../config/jwt-config";
+import { User } from "../models/user-model";
+
+interface TokenRequest {
+    username: string;
+    password: string;
+}
+
+interface StoreRequest {
+    email: string;
+    username: string;
+    fullname: string;
+    password: string;
+}
+
+export class UserController {
+    token() {
+        return async (req: Request, res: Response) => {
+            const { username, password }: TokenRequest = req.body;
+            if (!username || !password) {
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    message: ReasonPhrases.BAD_REQUEST,
+                });
+                return;
+            }
+
+            const user = await User.createQueryBuilder("user")
+                .select(["user.userID", "user.password", "user.isAdmin"])
+                .where("user.username = :username", { username })
+                .getOne();
+            if (!user) {
+                res.status(StatusCodes.UNAUTHORIZED).json({
+                    message: "Invalid credentials",
+                });
+                return;
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                res.status(StatusCodes.UNAUTHORIZED).json({
+                    message: "Invalid credentials",
+                });
+                return;
+            }
+
+            const { userID, isAdmin } = user;
+            const payload: AuthToken = {
+                userID,
+                isAdmin,
+            };
+            const token = jwt.sign(payload, jwtConfig.secret, {
+                expiresIn: jwtConfig.expiresIn,
+            });
+
+            res.status(StatusCodes.OK).json({
+                message: ReasonPhrases.OK,
+                token,
+            });
+        };
+    }
+
+    store() {
+        return async (req: Request, res: Response) => {
+            const { email, username, fullname, password }: StoreRequest = req.body;
+            if (!email || !username || !fullname || !password) {
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    message: ReasonPhrases.BAD_REQUEST,
+                });
+                return;
+            }
+
+            const user = new User();
+            user.email = email;
+            user.username = username;
+            user.fullname = fullname;
+            user.password = password;
+            user.isAdmin = false;
+
+            // Cek apakah data sudah ada ...
+            const existingUserWithUsername = await User.findOneBy({
+                username,
+            });
+            if (existingUserWithUsername) {
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    message: "Username already taken!",
+                });
+                return;
+            }
+
+            const existingUserWithEmail = await User.findOneBy({
+                email,
+            });
+            if (existingUserWithEmail) {
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    message: "Email already taken!",
+                });
+                return;
+            }
+
+            const newUser = await user.save();
+            if (!newUser) {
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    message: ReasonPhrases.BAD_REQUEST,
+                });
+                return;
+            }
+
+            const { userID, isAdmin } = newUser;
+            const payload: AuthToken = {
+                userID,
+                isAdmin,
+            };
+            const token = jwt.sign(payload, jwtConfig.secret, {
+                expiresIn: jwtConfig.expiresIn,
+            });
+
+            res.status(StatusCodes.CREATED).json({
+                message: ReasonPhrases.CREATED,
+                token,
+            });
+        };
+    }
+
+    index() {
+        return async (req: Request, res: Response) => {
+
+            const users = await User.createQueryBuilder("user")
+                .select(["user.userID", "user.fullname"])
+                .where("user.isAdmin = :isAdmin", { isAdmin: false })
+                .cache(
+                    `creator`,
+                    cacheConfig.cacheExpirationTime
+                )
+                .getMany()
+
+            res.status(StatusCodes.OK).json({
+                message: ReasonPhrases.OK,
+                data: users
+            });
+        };
+    }
+
+    admin() {
+        return async (req: Request, res: Response) => {
+
+            const admin = await User.findOneBy({
+                isAdmin: true,
+            });
+
+            res.status(StatusCodes.OK).json({
+                message: ReasonPhrases.OK,
+                data: admin,
+            });
+        };
+    }
+
+    check() {
+        return async (req: Request, res: Response) => {
+            const { token } = req as AuthRequest;
+            if (!token) {
+                res.status(StatusCodes.UNAUTHORIZED).json({
+                    message: ReasonPhrases.UNAUTHORIZED,
+                });
+                return;
+            }
+
+            res.status(StatusCodes.OK).json({
+                userID: token.userID,
+                isAdmin: token.isAdmin,
+            });
+        };
+    }
+}
